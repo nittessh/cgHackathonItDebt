be20525d1f0b43183b95b8cd5800a141a0899291
nittessh <147621328+nittessh@users.noreply.github.com>
2023-10-20 20:51:40 +0530

    Update DisplayLicense.js

--- frontEndReact/cg_it/src/components/DisplayLicense.js
+++ frontEndReact/cg_it/src/components/DisplayLicense.js
@@ -1,45 +1,5 @@
-// changed by nittessh
-
-/*
-import React from 'react';
-import LicenseData from './License.json';
-import './DisplayAssetDetails.css';
-
-const DisplayLicense = () => {
-  return (
-    <div className='data'>
-      <h1>Asset Details</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Asset ID</th>
-            <th>Asset Name</th>
-            <th>Provision Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {LicenseData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.asset_id}</td>
-              <td>{item.assetName}</td>
-              <td>{item.provisionDate}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default DisplayLicense;
-*/
-
 import React, { useState, useEffect } from 'react';
 
-const shouldHighlightRow = (currentRow, otherRow) => {
-    return currentRow.category === otherRow.category;
-  };
-  
 const DisplayAssetDetails = () => {
   const [data, setData] = useState([]);
 
@@ -57,40 +17,43 @@ const DisplayAssetDetails = () => {
     fetchData();
   }, []);
 
+  const shouldHighlightRow = (currentRow, index) => {
+    // Check if the category is repeated in the data array
+    const isRepeated = data.slice(0, index).some((otherRow) => otherRow.category === currentRow.category);
+    return isRepeated;
+  };
+
   return (
-    <div align="center">
+    <div className='data' align="center">
       <h1>Licenses Available</h1>
-      <table border="1" cellspacing="0" cellpadding="5">
+      <table border="1" cellSpacing="0" cellPadding="5">
         <thead>
           <tr>
             <th>ASSET ID</th>
             <th>ASSET NAME</th>
             <th>PROVISION DATE</th>
-			<th>PATCHED DATE</th>
-			<th>License Category</th>
+            <th>PATCHED DATE</th>
+            <th>License Category</th>
           </tr>
         </thead>
         <tbody>
-          
-		  
-		  {data.map((currentRow, currentIndex) => (
-            <tr key={currentRow.asset_id}>
+          {data.map((currentRow, currentIndex) => (
+            <tr
+              key={currentRow.asset_id}
+              style={shouldHighlightRow(currentRow, currentIndex) ? { background: 'yellow' } : null}
+            >
               <td>{currentRow.asset_id}</td>
               <td>{currentRow.assetName}</td>
-			  <td>{currentRow.provisionDate}</td>
-			  <td>{currentRow.patchedDate}</td>
-              <td>
-			  {currentRow.category}
-			  </td>
+              <td>{currentRow.provisionDate}</td>
+              <td>{currentRow.patchedDate}</td>
+              <td>{currentRow.category}</td>
             </tr>
           ))}
-		  
         </tbody>
       </table>
-	  <br/><br/>
+      <br/><br/>
     </div>
   );
 };
 
 export default DisplayAssetDetails;
-
